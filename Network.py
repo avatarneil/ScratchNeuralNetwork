bcce887de44e3b785089f7ecf34239470c9730b3
Neil Goldader <Neil Goldader>
2016-11-11 00:40:47 -0500

    more stuff

--- Network.py
+++ Network.py
@@ -1,13 +1,20 @@
 import numpy as np
-x = [1,2,3,4,5,6]
-z = [1,4,9,16,25,36]
+import matplotlib.pyplot as plt
+x = np.linspace(1,50,50)
+z= np.linspace(1,50,50)
+while i<=len(x):
+    z[i]=x[i]**2
+    
 step = .01
+i=0
+wumbo1 = np.random.rand(1)
+wumbo2 = np.random.rand(1)
+w1=wumbo1[0]
+w2=wumbo2[0]
+#cs = np.array([])
 
 def sigmoid(a):
     return (1/(1+np.exp(-a)))
-
-w1 = np.random(0,1)
-w2 = np.random(0,1)
 
 def calcVar(a):
     p1 = (w1*a)
@@ -16,12 +23,19 @@ def calcVar(a):
     return sigmoid(p2)
 
 def training():
-    while i<len(z):
+    global i
+    global w1
+    global w2
+    while (i<=len(z)):
         v = calcVar(x[i])
-        c=(z[i]-v)^2
-        dcw1 = 
-        dcw2 = 2*(z[i]-v)*v*(1-v)*
-        w1 = w1-(dcw1*step)
-        w2 = w2-(dcw2*step)
-        i++
-        
+        y = sigmoid(w1*x[i])
+        c=(z[i]-v)**2
+        dcw1 = 2*(z[i]-v)*v*(1-v)*w2*y*(1-y)*x[i]
+        dcw2 = 2*(z[i]-v)*v*(1-v)*y
+        w1 =w1-(dcw1*step)
+        w2 =w2-(dcw2*step)
+        print(c)
+        #plt.plot(i,c)
+        i+=1
+
+training()
