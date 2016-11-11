2e426bee9d1806182045fcf84b87001b58e2bfbd
Neil Goldader <Neil Goldader>
2016-11-11 09:03:39 -0500

    started working on a PyBrain based alternative

---
+++ PyBrainNetwork.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from pybrain.tools.shortcuts import buildNetwork
+ 
+np.random.seed(19230834)
+x = np.linspace(0,1,100)
+z= np.linspace(0,1,100)
+i=0
