reverted some prior commits, trying to find a way to fix gradient descent function.

Network.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 np.random.seed(19230834)
 x = np.linspace(0,1,100)
-z = np.linspace(0,1,100)
+z= np.linspace(0,1,100)
 i=0
 while i<len(x):
     z[i]=2*x[i]
@@ -31,14 +31,13 @@ def training():
     while (i<len(z)):
         v = calcVar(x[i])
         y = sigmoid(w1*x[i])
-        c=(z[i] - v)
-        dcw1 =v*(1-v)*y*(1-y)
-        dcw2 =v*(1-v)
+        c=(z[i]-v)
+        dcw1 =2*(z[i]-v)*v*(1-v)*w2*y*(1-y)*x[i]
+        dcw2 =2*(z[i]-v)*v*(1-v)*y
         w1 =w1-(dcw1*c)
         w2 =w2-(dcw2*c)
         #plt.plot(i,c)
-        print(c,w1,w2)
+        print(c)
         i+=1
     i=0
 training()
-print(calcVar(.5))

PyBrainNetwork.py
@@ -5,21 +5,17 @@ from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 
 np.random.seed(19230834)
-x = np.linspace(0,1,10)
-z = np.linspace(0,1,10)
-i=0
-while i<len(x):
-    z[i]=x[i]**2
-    i+=1
-i=0
-ds = SupervisedDataSet(1, 1)
-net = buildNetwork(1, 3, 1, bias=True)
-while i<len(x):
-    ds.addSample((x[i]),(z[i],))
-    i+=1
-i=0
-
+ds = SupervisedDataSet(2, 1)
+net = buildNetwork(2, 1000, 1, bias='True')
+#while i<len(x):
+   # ds.addSample((x[i]),(z[i],))
+  #  i+=1
+#i=0
+ds.addSample((0,0),(0,))
+ds.addSample((0,1),(1,))
+ds.addSample((1,0),(1,))
+ds.addSample((1,1),(0,))
 trainer = BackpropTrainer(net, ds)
 trainer.trainUntilConvergence()
 
-print(net.activate([3]))
+print(net.activate([0,1]))