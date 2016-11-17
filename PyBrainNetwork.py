43fa54c84b0af6ceb32d7d8d794d13aea0b35538
Neil Goldader <Neil Goldader>
2016-11-16 21:56:14 -0500

    lul i hate my life

--- PyBrainNetwork.py
+++ PyBrainNetwork.py
@@ -3,19 +3,14 @@ import matplotlib.pyplot as plt
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
+import csv
 
 np.random.seed(19230834)
-ds = SupervisedDataSet(2, 1)
-net = buildNetwork(2, 1000, 1, bias='True')
-#while i<len(x):
-   # ds.addSample((x[i]),(z[i],))
-  #  i+=1
-#i=0
-ds.addSample((0,0),(0,))
-ds.addSample((0,1),(1,))
-ds.addSample((1,0),(1,))
-ds.addSample((1,1),(0,))
-trainer = BackpropTrainer(net, ds)
-trainer.trainUntilConvergence()
 
-print(net.activate([0,1]))
+inputNumber=1
+outputNumber=1
+ds = SupervisedDataSet(inputNumber, outputNumber)
+net = buildNetwork(2,5,5,1)
+
+trainer = BackpropTrainer(net, ds)
+print(trainer.trainUntilConvergence())
